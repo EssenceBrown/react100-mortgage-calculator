0ef49c97a6fd13636c6d3dd0ccbf397ebc8212db
EssenceBrown <essence@cccareers.org>
2022-11-02 16:26:23 -0400

    deleted old code

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -125,15 +125,7 @@ export default class App extends React.Component {
 
 
                 <button onClick={this.handleClick}>Submit</button>
-                {/* <div
-                    id="output"
-                  >
-                   {''}
-                    {new Intl.NumberFormat('dollars', {
-                      style: 'currency',
-                      currency: 'USD'
-                  }).format(this.state.output)}     
-                  </div> */}
+
 
               </div>
             </div>
@@ -145,63 +137,7 @@ export default class App extends React.Component {
         </div>
 
 
-        {/* <button type="button" classNameName="btn btn-primary center" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch Calculator</button>
 
-  
-        <div classNameName="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title fs-5" id="exampleModalLabel"> Determine the financial implications of changes in one or more variables in a mortgage financing arrangement.</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                <input
-                  id="balance"
-                  name="balance"
-                  type="number"
-                  placeholder="Loan Amount"
-                  onChange={this.handleChange}
-                />
-                <input
-                  id="rate"
-                  name="rate"
-                  type="number" step="0.01"
-                  placeholder="rate%"
-                  onChange={this.handleChange}
-                />
-                <select
-                  id="select"
-                  name="term"
-                  onChange={this.handleChange}
-                >
-                  <option value="" disabled="disabled">Select Term</option>
-                  <option value={15}>15</option>
-                  <option value={30}>30</option>
-                </select>
-
-                <div className="modal-footer">
-
-                  <button
-                    id="submit"
-                    name="submit"
-                    type="submit"
-                    onClick={this.handleClick}>Click to submit</button>
-
-                  <div
-                    id="output"
-                  >
-                    your monthly payment is{''}
-                    {new Intl.NumberFormat('dollars', {
-                      style: 'currency',
-                      currency: 'USD'
-                    }).format(this.state.output)}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </div>
     );
   }
