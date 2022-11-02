8931c804c7e1a9bd9172c50671f49dc4c12601c9
EssenceBrown <essence@cccareers.org>
2022-11-01 22:22:12 -0400

    added a condtional Rendering to button

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -12,6 +12,7 @@ export default class App extends React.Component {
       rate: 0,
       term: 0,
       output: '',
+      showMP: false,
 
     };
 
@@ -36,24 +37,110 @@ export default class App extends React.Component {
     const principle = this.state.balance;
     const m = ((principle * (r * Math.pow(1 + r, n))) / (Math.pow(1 + r, n) - 1)).toFixed(2);
 
-    this.setState({ output: m });
+    this.setState({ output: m , showMP :! this.state.showMP });
     //console.log(n,r,m)
   }
 
-
-
+   
 
   render() {
-
+ 
     return (
       <div className="container">
-          <img src="/images/home.png" alt=""/>
-          <h1 >Mortgage Calculator</h1>
 
-        <button type="button" className="btn btn-primary center" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch Calculator</button>
+        {/* <img src="/images/home.png" alt="" /> */}
+
+        <h1 className="card-title">Mortgage Calculator</h1>
+        <h2>Simply enter the loan amount, interest rate and term in the fields below and click submit. </h2>
+
+        <div className="card" >
+
+            {/* <h1 className="card-title">Mortgage Calculator</h1> */}
+          <div className="card-body" id="cardStyle">
+
+<div className="row">
+<div className="col-6">
+<div className="input-group mb-31" >
+              <span className="input-group-text" id="inputGroup-sizing-default">Loan Amount</span>
+              <input
+                className="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-default"
+                id="balance"
+                name="balance"
+                type="number"
+                placeholder="Enter Amount"
+                onChange={this.handleChange}
+              />
+              </div>
+
+              <div className="input-group mb-32" >
+                <span className="input-group-text" id="inputGroup-sizing-default">Interest Rate</span>
+                <input
+                  className="form-control"
+                  aria-label="Sizing example input"
+                  aria-describedby="inputGroup-sizing-default"
+                  id="rate"
+                  name="rate"
+                  type="number"
+                  step="0.01"
+                  placeholder=" Enter %"
+                  onChange={this.handleChange}
+                />
+                </div>
+
+                <div className="input-group mb-33" >
+                  <span className="input-group-text" id="inputGroup-sizing-default">Loan Term</span>
+                  <select type="select"
+                    className="form-control"
+                    aria-label="Sizing example input"
+                    aria-describedby="inputGroup-sizing-default"
+                    id="select"
+                    name="term"
+                    onChange={this.handleChange}
+                  >
+                    <option value="">Select Term</option>
+                    <option value={15}>15</option>
+                    <option value={30}>30</option>
+                  </select>
+                  </div>
+
+</div>
+<div className="col-6">
+
+  <div>
+   
+    {this.state.showMP ? this.state.output : <img src="/images/BFlogo.png" />}
+   
+                   
+  </div>
+
+  {/* <button onClick={this.handleClick}>Submit</button> */}
+                  <button  onClick={this.handleClick}>Submit</button>
+                  {/* <div
+                    id="output"
+                  >
+                   {''}
+                    {new Intl.NumberFormat('dollars', {
+                      style: 'currency',
+                      currency: 'USD'
+                  }).format(this.state.output)}     
+                  </div> */}
+
+</div>
+</div>
+
+
+               
+
+                </div>
+              </div>
+        
+        
+        {/* <button type="button" classNameName="btn btn-primary center" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch Calculator</button>
 
   
-        <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div classNameName="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
@@ -106,7 +193,7 @@ export default class App extends React.Component {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
